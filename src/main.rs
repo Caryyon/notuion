c33b1576798a88232153abe4e06b8dced049f924
Cary Wolff <boss@caryyon.com>
2022-05-22 20:45:04 -0500

    not much mostly ascii art

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-// =============================================
-//                   NOTUION
-// =============================================
-//
-// lets start by just cURLing the Notion API
+/* ==========================================
+     __  ___  _____        _____  ___    __
+  /\ \ \/___\/__   \/\ /\  \_   \/___\/\ \ \
+ /  \/ //  //  / /\/ / \ \  / /\//  //  \/ /
+/ /\  / \_//  / /  \ \_/ /\/ /_/ \_// /\  /
+\_\ \/\___/   \/    \___/\____/\___/\_\ \/
+========================================== */
 
 use serde_derive::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MyConfig {
-    version: u8,
+    version: String,
     api_key: String,
 }
 
 /// `MyConfig` implements `Default`
 impl ::std::default::Default for MyConfig {
-    fn default() -> Self { Self { version: 0, api_key: "".into() } }
+    fn default() -> Self { Self { version: "0".into(), api_key: "".into() } }
 }
 
 fn main() -> Result<(), confy::ConfyError> {
